747e3a437ba70ec17eb262950235bc26b57b4bd8
Tedris <vanloone@gmail.com>
2018-08-19 15:45:34 -0400

    Actually update the number

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 lootBoxElementById = document.getElementById("lootBoxes");
 
 function openLootBox(numberOpened) {
-    var lootBoxes = lootBoxElementById.innerHTML;
+    var lootBoxes = parseFloat(lootBoxElementById.innerHTML);
+    lootBoxes = lootBoxes + numberOpened;
+    updateLootBoxes(lootBoxes);
+}
+
+function updateLootBoxes(lootBoxNum) {
+    lootBoxElementById.innerHTML = lootBoxNum;
 }
