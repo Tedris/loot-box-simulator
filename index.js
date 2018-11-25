b63ccef56ba937b9cfe955f36965f8dd9358cd64
Tedris <vanloone@gmail.com>
2018-11-25 13:38:19 -0500

    Adding Level and Experience

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-var lootBoxElementById = document.getElementById("lootBoxes");
-
 var lootBoxes = 0;
+var level = 1;
+var experience = 0;
 
-function openLootBox(numberOpened) {
-    lootBoxes = parseFloat(lootBoxElementById.innerHTML);
-    lootBoxes = lootBoxes + numberOpened;
-    updateLootBoxes(lootBoxes);
+function addToObject(objectName, number) {
+    var object = window[objectName];
+    object = parseInt(object) + number;
+    updateObject(objectName, object);
 }
 
-function updateLootBoxes(lootBoxNum) {
-    lootBoxElementById.innerHTML = lootBoxNum;
+function updateObject(objectName, number) {
+    document.getElementById(objectName).innerHTML = number;
+    window[objectName] = number;
 }
 
 function save() {
     localStorage.setItem("lootBoxes", lootBoxes);
+    localStorage.setItem("level", level);
+    localStorage.setItem("experience", experience);
 }
 
 function load() {
     lootBoxes = localStorage.getItem("lootBoxes");
-    updateLootBoxes(lootBoxes || 0);
+    level = localStorage.getItem("level");
+    experience = localStorage.getItem("experience");
+    updateObject("lootBoxes", lootBoxes || 0);
+    updateObject("level", level || 1);
+    updateObject("experience", experience || 0);
 }
 
 load();
